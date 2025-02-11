30b5f662caf71174a0044b332fe177f807bec047
TheLastJedi00 <jediaelborges23@gmail.com>
2025-02-11 09:25:36 -0300

    Criação da Entidade Paciente
    Criação da Entidade Paciente: Implementada a classe Paciente, definindo seus atributos e construtor, utilizando o record DadosCadastroPaciente.
    
    Implementação do Controller: Criada a classe PacienteController, que inclui o método para cadastrar pacientes, utilizando o repositório para salvar os dados.
    
    Definição dos Records: Definidos os records DadosCadastroPaciente e DadosEndereco, garantindo que os campos necessários fossem validados corretamente.
    
    Criação da Tabela no Banco de Dados: Criada a migration para a tabela pacientes, garantindo que a estrutura estivesse correta e que a tabela fosse criada no banco de dados.
    
    Execução da Migration: Migration aplicada com sucesso, criando a tabela pacientes no banco de dados.

--- api/src/main/java/med/voll/api/paciente/PacienteController.java
+++ api/src/main/java/med/voll/api/paciente/PacienteController.java
@@ -1,15 +1,26 @@
 package med.voll.api.paciente;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/pacientes")
+
 public class PacienteController {
 
-    public void paciente(@RequestBody DadosCadastroPaciente dados){
-        System.out.println(dados);
-    }
+    @Autowired
+    private PacienteRepository repository;
 
+    @PostMapping
+    @Transactional
+
+    public void paciente(@RequestBody @Valid DadosCadastroPaciente dados){
+        repository.save(new Paciente(dados));
+    }
 }
