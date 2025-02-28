65e3cb8f68b891d316094e107fe59616fe7608a6
TheLastJedi00 <jediaelborges23@gmail.com>
2025-02-28 09:26:40 -0300

    Paginação
    No método listar() do MedicoController, foi adicionado um parâmetro do tipo Pageable para controlar a paginação.
    O retorno do método listar() foi alterado de List para Page para incluir as informações de paginação.

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,8 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,8 +29,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> Listar(){
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> Listar(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
 }
