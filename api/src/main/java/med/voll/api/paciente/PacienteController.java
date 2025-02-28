d9d03e09b9de832fe7a2141099247abcb852879e
TheLastJedi00 <jediaelborges23@gmail.com>
2025-02-28 11:05:54 -0300

    Paginação agora disponível em lista de pacientes

--- api/src/main/java/med/voll/api/paciente/PacienteController.java
+++ api/src/main/java/med/voll/api/paciente/PacienteController.java
@@ -3,11 +3,12 @@ package med.voll.api.paciente;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.MedicoRepository;
+import org.aspectj.apache.bcel.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -20,7 +21,12 @@ public class PacienteController {
     @PostMapping
     @Transactional
 
-    public void paciente(@RequestBody @Valid DadosCadastroPaciente dados){
+    public void paciente(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> Listar(@PageableDefault(sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
