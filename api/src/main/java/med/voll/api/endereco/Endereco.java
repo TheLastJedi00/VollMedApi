22e6ec1946106208eea4675ceb51bf67397e9361
TheLastJedi00 <jediaelborges23@gmail.com>
2025-03-01 01:41:06 -0300

    Capacidade de atualizar dados implementada
    Criação do DTO DadosAtualizacaoMedico:
    Esse DTO será usado para atualizar os dados do médico, ao invés de usar o mesmo DTO de cadastro (DadosCadastroMedico).
    Ele contém apenas os campos atualizáveis: id, nome, telefone e endereco.
    O campo id foi marcado como @NotNull para indicar que é obrigatório.
    
    Implementação do método atualizarInformacoes() na classe Medico:
    Esse método atualiza os dados do médico com base nos valores recebidos no DTO DadosAtualizacaoMedico.
    Ele verifica se cada campo do DTO possui um valor não nulo antes de atualizar o respectivo campo do objeto Medico.
    
    Implementação do método atualizarInformacoes() na classe Endereco:
    Esse método atualiza os dados do endereço com base nos valores recebidos no DTO DadosEndereco.
    Ele também verifica se cada campo do DTO possui um valor não nulo antes de atualizar o respectivo campo do objeto Endereco.
    
    Atualização do método update() no MedicoController:
    Ele carrega o objeto Medico do banco de dados usando o id recebido no DTO.
    Em seguida, chama o método atualizarInformacoes() do objeto Medico passando o DTO DadosAtualizacaoMedico como parâmetro.
    Não é necessário salvar manualmente o objeto Medico atualizado, pois a JPA cuida disso automaticamente.

--- api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -30,4 +30,28 @@ public class Endereco {
 
 
     }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if(dados.logradouro() != null){
+            this.logradouro = dados.logradouro();
+        }
+        if(dados.bairro() != null){
+            this.bairro = dados.logradouro();
+        }
+        if(dados.cep() != null){
+            this.cep = dados.logradouro();
+        }
+        if(dados.cidade() != null){
+            this.cidade = dados.logradouro();
+        }
+        if(dados.uf() != null){
+            this.uf = dados.logradouro();
+        }
+        if(dados.complemento() != null){
+            this.complemento = dados.logradouro();
+        }
+        if(dados.numero() != null){
+            this.numero = dados.logradouro();
+        }
+    }
 }
--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -1,10 +1,12 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import med.voll.api.endereco.DadosEndereco;
 import med.voll.api.endereco.Endereco;
 
 @Table(name = "medicos")
@@ -37,5 +39,18 @@ public class Medico {
         this.endereco = new Endereco(dados.endereco());
         this.especialidade = dados.especialidade();
         this.telefone = dados.telefone();
+    }
+
+    public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
+
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
     }
 }
