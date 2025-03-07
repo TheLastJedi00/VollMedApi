5619c112eba29b7d5e0d0d9ea6c0b3b34de6a916
TheLastJedi00 <jediaelborges23@gmail.com>
2025-03-07 08:19:58 -0300

    Correção no retorno de códigos (Protocolo HTTP)
    Alteração no Método de Cadastro:
    O método cadastrar foi modificado para retornar um ResponseEntity em vez de void.
    Implementação do retorno do código HTTP 201 Created ao cadastrar um novo médico.
    Adição de um cabeçalho Location na resposta, que indica a URI do novo recurso criado.
    
    Criação da URI:
    Utilização da classe UriComponentsBuilder para construir a URI do novo médico.
    O método path foi utilizado para definir o caminho da URI, incluindo um parâmetro dinâmico {id}.
    O uso de buildAndExpand(medico.getId()).toUri() para substituir o {id} pelo ID do médico que foi criado.
    
    Retorno de Dados no Corpo da Resposta:
    O corpo da resposta agora inclui um objeto DadosDetalhamentoMedico, que representa os dados do médico recém cadastrado.
    
    Testes de Funcionalidade:
    Testes realizados para verificar se as operações de cadastro, listagem, atualização e exclusão estão funcionando corretamente e retornando os códigos de status apropriados.

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import com.mysql.cj.x.protobuf.Mysqlx;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.*;
@@ -7,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -21,27 +24,42 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
-        //requisição de valores
-        repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder){
+        //Salvar(novo médicos(atributos))
+        var medico = new Medico(dados);
+        repository.save(medico);
+        //Uri bilder cria URI
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+        //201 Created
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> Listar(@PageableDefault(size = 5, sort = {"nome"}) Pageable paginacao){
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity<Page<DadosListagemMedico>> Listar(@PageableDefault(size = 5, sort = {"nome"}) Pageable paginacao){
+        //Paginação de ativos
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        //200 OK
+        return ResponseEntity.ok(page);
+
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        //Bucar ID
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+        //200 OK
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id){
+    public ResponseEntity excluir(@PathVariable Long id){
+        //Buscar ID
         var medico = repository.getReferenceById(id);
         medico.excluir();
+        //204 Delete
+        return ResponseEntity.noContent().build();
     }
 }
