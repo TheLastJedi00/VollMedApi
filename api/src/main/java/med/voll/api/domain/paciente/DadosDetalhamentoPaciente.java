98a076f9e1edb1b4a6dcde447c648fa12535b96c
TheLastJedi00 <jediaelborges23@gmail.com>
2025-03-09 12:29:32 -0300

    Tratamento de erros e SpringSecurity
    Erro 404 e 400 corrigidos

---
+++ api/src/main/java/med/voll/api/domain/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,20 @@
+package med.voll.api.domain.paciente;
+
+import med.voll.api.domain.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(
+        Long id,
+        String nome,
+        String email,
+        String cpf,
+        Endereco endereco
+) {
+    public DadosDetalhamentoPaciente (Paciente paciente){
+        this
+                (paciente.getId(),
+                paciente.getNome(),
+                paciente.getEmail(),
+                paciente.getCpf(),
+                paciente.getEndereco());
+    }
+}
