77bce13c451aa28a9eaeb63d6a2500a3b6509af3
TheLastJedi00 <jediaelborges23@gmail.com>
2025-03-09 15:36:00 -0300

    Desabilitada proteção CSRF
    Autenticação STATELESS

---
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,30 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
+        //cross-site request forgery
+        return http.
+                csrf(csrf -> csrf.disable()).
+                sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).
+                build();
+    }
+
+    /*public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http.csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().build();
+    } Esse método está desatualizado*/
+}
