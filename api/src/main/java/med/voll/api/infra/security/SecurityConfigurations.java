c0c750244215e96991d1f254c23898dfed87c4ad
TheLastJedi00 <jediaelborges23@gmail.com>
2025-03-09 21:26:53 -0300

    Controller de Autenticação
    Criação do AutenticacaoController:
    Foi criado um novo controller (AutenticacaoController) para receber as requisições de login no endpoint /login.
    Esse controller utiliza o método efetuarLogin para receber o login e senha do usuário, autenticá-lo e, eventualmente, retornar um token JWT (que será implementado em aulas futuras).
    
    Criação do DTO DadosAutenticacao:
    Foi criado um DTO (DadosAutenticacao) para representar os dados de autenticação (login e senha) recebidos na requisição.
    
    Configuração do AuthenticationManager:
    Na classe SecurityConfigurations, foi configurado o AuthenticationManager para gerenciar o processo de autenticação.
    Foi adicionado um método anotado com @Bean para expor o AuthenticationManager para o Spring.
    
    Configuração do PasswordEncoder:
    Na classe SecurityConfigurations, foi configurado o PasswordEncoder para utilizar o algoritmo BCrypt para codificar e verificar as senhas.
    Foi adicionado um método anotado com @Bean para expor o PasswordEncoder para o Spring.
    
    Implementação da interface UserDetails na classe Usuario:
    A classe Usuario foi modificada para implementar a interface UserDetails do Spring Security.
    Os métodos da interface UserDetails foram implementados para fornecer informações sobre o usuário (nome de usuário, senha, authorities, status da conta).
    
    Adição de usuário no banco de dados:
    Foi inserido um usuário de exemplo no banco de dados com a senha codificada utilizando o BCrypt.

--- api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,11 +2,15 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,4 +31,13 @@ public class SecurityConfigurations {
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and().build();
     } Esse método está desatualizado*/
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
